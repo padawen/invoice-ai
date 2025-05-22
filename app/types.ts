31bed676523b31222ca1695ccd6232e0014e095f
padawen <daveherczeg@gmail.com>
2025-05-22 20:35:27 +0200

    Made the database more chill by letting it accept optional fields. It's now as laid-back as a hammock on a sunny day!

--- app/types.ts
+++ app/types.ts
@@ -11,21 +11,21 @@ export interface EditableInvoice {
   id: string;
   seller: {
     name: string;
-    address: string;
-    tax_id: string;
-    email: string;
-    phone: string;
+    address?: string;
+    tax_id?: string;
+    email?: string;
+    phone?: string;
   };
   buyer: {
     name: string;
-    address: string;
-    tax_id: string;
+    address?: string;
+    tax_id?: string;
   };
-  invoice_number: string;
-  issue_date: string;
-  fulfillment_date: string;
-  due_date: string;
-  payment_method: string;
-  currency: string;
+  invoice_number?: string;
+  issue_date?: string;
+  fulfillment_date?: string;
+  due_date?: string;
+  payment_method?: string;
+  currency?: string;
   invoice_data: InvoiceData[];
 }
